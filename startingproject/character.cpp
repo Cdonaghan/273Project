3054702f17e0339bc97b51c042b354bd684d4aaa
santirivett <santirivett@yahoo.com>
2023-02-23 12:54:54 +0000

    stuff for project

---
+++ startingproject/character.cpp
@@ -0,0 +1,27 @@
+#include"character.h"
+#include"game.h"
+character::character()
+{
+	name = { "" };
+	level = 0;
+	exp = 0;
+	health = 0;
+	maxhealth = 200;
+
+}
+character::~character()
+{
+
+}
+string character::getname() const
+{
+	return name;
+}
+void character::setname(string x)
+{
+	x = name;
+}
+int character::level()
+{
+	return level;
+}
---
+++ startingproject/main.cpp
@@ -0,0 +1,13 @@
+#include"game.h"
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	Game game;
+	while (game.getplaying()==true)
+	{
+		game.mainmenu();
+		cout << "hello cunt" << endl;
+	}
+}
