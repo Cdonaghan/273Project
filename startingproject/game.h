abf9815dc369ebe06e87c2a23e51161c690f3fc2
santirivett <santirivett@yahoo.com>
2023-02-27 13:17:53 +0000

    update

--- startingproject/game.h
+++ startingproject/game.h
@@ -10,10 +10,14 @@ class Game
 public:
 	Game();
 	virtual ~Game();
+	void initgame()
 	void mainmenu();
 	bool getplaying() const;
 
 private:
 	int choice;
 	bool playing;
+	//character creation
+
+	character main;
 };
